768d5f7de851c4194c791ef8f9939d77c79cdce2
Christina Cook <cfcook@ucsd.edu>
2017-02-09 19:57:59 -0800

    finished both columns

--- script.js
+++ script.js
@@ -39,16 +39,18 @@
 
 // console.log(timesThree);
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $("#show").click(function(){
-        $("p").show();
+//     $("#show").click(function(){
+//         $("p").show();
 
-    });
+//     });
 
 
 
-});
+// });
+
+
 
 $('#showEmail').on('click','.tripName',function() {
         
@@ -58,4 +60,32 @@ $('#showEmail').on('click','.tripName',function() {
          
        
 });
+
+// $('#hoverelement').hover(function(){
+// 	$main_text=$(this).text();
+// 	$(this).text("Create New Plan");
+// },function(){
+// 	$(this).text($main_text);
+// 	});
+
+var intTextBox = 1; 
+
+function addElement() {
+    intTextBox++;
+    var objNewDiv = document.createElement('div');
+    objNewDiv.setAttribute('id', 'div_' + intTextBox);
+     objNewDiv.innerHTML = ' ' + ' <p style = "color: #ff5d55;" >' + ' <input type="email" id="email" name="email" required placeholder="Enter a valid email address"> </p>';
+    document.getElementById('content').appendChild(objNewDiv);
+}
+
+
+function removeElement() {
+    if(0 < intTextBox) {
+        document.getElementById('content').removeChild(document.getElementById('div_' + intTextBox));
+        intTextBox--;
+    } else {
+        alert("No textbox to remove");
+    }
+}
+
 
