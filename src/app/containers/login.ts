36f0814a8c0291d5b2f16219fbd44d62db9e9e3f
Christina Cook <cfcook@ucsd.edu>
2017-02-16 00:59:34 -0800

    added login + add trip page

--- src/app/containers/login.ts
+++ src/app/containers/login.ts
@@ -2,28 +2,29 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'login-container',
-    styles: [`
-    form {
-        background-color: white;
-        display: inline-block;
-    }
-    
-  `],
+    styleUrls: [`app/containers/login.component.css`],
     template: `
-    <div class="login-container">
+
+    <div class = "container">
+
+            <!--<div class = "tiny-hex"> </div>-->
     
-      <h1>Login Page</h1>
+        <div id = "section-title">
+            <h1> <b>B-STORM </b></h1>
+            <p id = "slogan"> A Brainstorming Application for Planning Trips <p>
+            <div id = "form">
+                <input type="text" id="name" name="name" required placeholder="NAME"> <p>
+                <input type="email" id="email" name="email" required placeholder="EMAIL ADDRESS"><p>
+                <input type="text" id="pw" name="pw" required placeholder=" PASSWORD"> <p>
+                <a href="add-trip">
+                    <input type="submit" class = "button-login" value = "START B-STORMING">
+                </a>
+            </div>
+        </div>
+        
+            <div class="hexagon"></div>
 
-    <h3> Welcome </h3>
-    
-    <form>
-        <input id = "email" type="text" required placeholder="Enter your email"> 
-        <input id = "pass" type="text" required placeholder="********"> 
-    </form>
-	<input type="submit" class ="btn btn-primary" value = "Login!">
-
-
-    </div>
+    </div> 
     `
 }) export class Login {}
 
