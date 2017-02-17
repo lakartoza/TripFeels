43c6213ded0dcab375234856bfaaaa1b5ae540d6
Valentine Bondar <vbondar@ucsd.edu>
2017-02-16 22:52:44 -0800

    made files compile. Removed gridts

--- src/app/containers/index.ts
+++ src/app/containers/index.ts
@@ -1,5 +1,5 @@
 export { Main } from './main';
-export { Notes } from './notes';
+export { Notes } from './notes/notes';
 export { About } from './about';
 export { Trips } from './trips';
 export { Login } from './login';
