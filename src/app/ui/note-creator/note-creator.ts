96b0a6a1615950489a36a8a5d68b751120eb9628
Christina Cook <cfcook@ucsd.edu>
2017-03-01 01:37:11 -0800

    button from login -> next page works, words on notes still doesn't show up

--- src/app/ui/note-creator/note-creator.ts
+++ src/app/ui/note-creator/note-creator.ts
@@ -11,20 +11,56 @@ import {
  
 })
 export class NoteCreator {
-  @Output() createNote = new EventEmitter();
+ 
+ @Output() createNote = new EventEmitter();
   colors: Array<string> = ['#B19CD9', '#FF6961', '#77DD77', '#AEC6CF', '#F49AC2', 'white'];
+  
+  // Data of all images
+  // private hex1= {
+  //   photo: '1.png'
+  // };
+  // private hex2 = {
+  //   photo: '2.png'
+  // };
+  // private hex3= {
+  //   photo: '3.png'
+  // };
+  // private hex4 = {
+  //   photo: '4.png'
+  // };
+  // private hex5= {
+  //   photo: '5.png'
+  // };
+  // private hex6 = {
+  //   photo: '.png'
+  // };
+
+  // private activity1 = this.hex1;
+  // private activity2 = this.hex2;
+  // private activity3 = this.hex3;
+  // private activity4 = this.hex4;
+  // private activity5 = this.hex5;
+  // private activity6 = this.hex6;
+
+
+
   newNote = {
     title: '',
     value: '',
     color: 'white'
   };
+
   fullForm: boolean = false;
 
   onCreateNote() {
     const { title, value, color } = this.newNote;
 
     if (title && value) {
-      this.createNote.next({ title, value, color });
+      // var colorChoice = []; 
+      // if (this.images[0] && this.colors[0]) {
+
+      this.createNote.next({ title, value, color});
+
     }
 
     this.reset();
