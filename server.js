62c44c93d9b2be966b6a6defa2fd839eddd27f46
Valentine Bondar <vbondar@ucsd.edu>
2017-02-28 18:16:56 -0800

    updated publicpath in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ module.exports = {
     const indexPath = path.join(__dirname, '/src/index.html');
     const publicPath = express.static(path.join(__dirname, '/dist'));
 
-    app.use('/dist', publicPath);
+    app.use('/', publicPath);
     app.get('/', function (_, res) { res.sendFile(indexPath) });
 
     return app;
