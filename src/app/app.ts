9079050f58aa4a82858d6eafd41398d05edd74ad
Valentine Bondar <vbondar@ucsd.edu>
2017-03-09 08:24:15 -0800

    removed dist folder

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,35 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  styleUrls: ['/src/app/app.css'],
+  styles: [`
+/*Loader*/
+
+.spinner {
+  margin: 100px auto 0;
+  width: 70px;
+  text-align: center;
+}
+
+.spinner > div {
+  width: 18px;
+  height: 18px;
+  background-color:  #ffc957;
+
+  border-radius: 100%;
+  display: inline-block;
+  -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+  animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+}
+
+.spinner .bounce1 {
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+
+.spinner .bounce2 {
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}`],
   template: `
     <div>
       <router-outlet></router-outlet>
