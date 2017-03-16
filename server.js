b47170533f5c5b1d5cb5962f132e33eb5d1b76cc
Valentine Bondar <vbondar@ucsd.edu>
2017-03-16 15:18:51 -0700

    Added the rest of server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ module.exports = {
     // CONTACTS API ROUTES BELOW
 
     // Generic error handler used by all endpoints.
-    function handleError(res, reason, message, code) {
+      function handleError(res, reason, message, code) {
       console.log("ERROR: " + reason);
       res.status(code || 500).json({"error": message});
     }
@@ -60,10 +60,11 @@ module.exports = {
           res.status(200).json(docs);
         }
       });
-
+    });
 
     app.post("/api/contacts", function(req, res) {
       var newContact = req.body;
+      newContact.createDate = new Date();
 
       if (!req.body.name) {
         handleError(res, "Invalid user input", "Must provide a name.", 400);
@@ -85,12 +86,37 @@ module.exports = {
     */
 
     app.get("/api/contacts/:id", function(req, res) {
+      db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+        if (err) {
+          handleError(res, err.message, "Failed to get contact");
+        } else {
+          res.status(200).json(doc);
+        }
+      });
     });
 
     app.put("/api/contacts/:id", function(req, res) {
+      var updateDoc = req.body;
+      delete updateDoc._id;
+
+      db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+        if (err) {
+          handleError(res, err.message, "Failed to update contact");
+        } else {
+          updateDoc._id = req.params.id;
+          res.status(200).json(updateDoc);
+        }
+      });
     });
 
     app.delete("/api/contacts/:id", function(req, res) {
+      db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+        if (err) {
+          handleError(res, err.message, "Failed to delete contact");
+        } else {
+          res.status(200).json(req.params.id);
+        }
+      });
     });
   });
     return app;
